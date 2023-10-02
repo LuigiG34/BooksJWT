4bd04dd3a3bbf4332071681098cbbb7e66cb69df
Luigi Gandemer <luigigandemer6@gmail.com>
2023-10-02 22:03:04 +0200

    Fixed SymfonyInsight suggestions

--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -21,36 +21,18 @@ class BookRepository extends ServiceEntityRepository
         parent::__construct($registry, Book::class);
     }
 
-    public function findAllWithPagination($page, $limit) {
+    /**
+     * Cette méthode retourne $limit livres à partir de la page $page. 
+     *
+     * @param integer $page
+     * @param integer $limit
+     * @return mixed
+     */
+    public function findAllWithPagination(int $page, int $limit) {
         $qb = $this->createQueryBuilder('b')
             ->setFirstResult(($page -1) * $limit)
             ->setMaxResults($limit);
 
         return $qb->getQuery()->getResult();
     }
-
-//    /**
-//     * @return Book[] Returns an array of Book objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('b')
-//            ->andWhere('b.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('b.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Book
-//    {
-//        return $this->createQueryBuilder('b')
-//            ->andWhere('b.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
